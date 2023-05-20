57134d4a11981a968fa745fe439cf0c9466ac161
Lucas Silverio <117397454+lukasilverio94@users.noreply.github.com>
2023-05-21 00:27:26 +0200

    add
    Some extra css at the list and some exercises that i Did during the course to solve by myself

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked by event listener");
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
